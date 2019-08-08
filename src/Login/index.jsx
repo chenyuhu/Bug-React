136b954b1aed4d85bd5b916791da05dde95a2f73
chenfeng <chenfeng@sishuxuefu.com>
2019-08-08 16:27:11 +0800

    修改文件

---
+++ src/Login/index.jsx
@@ -0,0 +1,22 @@
+/*
+ * @Author: 陈锋
+ * @Date: 2019-08-08 16:17:28
+ * @Last Modified by: 陈锋
+ * @Last Modified time: 2019-08-08 16:18:50
+ */
+
+import React, { Component } from 'react'
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() {
+    return (
+      <div>这个是login</div>
+    )
+  }
+}
+
+export default Login;
---
+++ src/app.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {  }
+  }
+  render() {
+    return (
+      <div>
+        {/* 这个是Home页面 */}
+      </div>
+    )
+  }
+}
+
+export default App
